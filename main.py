e39998e90e85161e48c5e928b65399c6b14a03d8
thetalhamirza <107436355+thetalhamirza@users.noreply.github.com>
2024-02-14 21:23:00 +0000

    Update main.py

--- main.py
+++ main.py
@@ -7,58 +7,107 @@ pfile = "passwords.txt"
 keydata = "keydata.txt"
 auth = False
 
+###
+
 def main():
     global auth
     if not auth:
-        initMasterKey()
-        userType = authorize()
-        
-        initializeKeys(userType)
+        initialize_user()
         auth = True
+    
+    print_menu()
+    choice = get_user_choice()
+    execute_choice(choice)
+
+def initialize_user():
+    initMasterKey()
+    user_type = authorize()
+    initializeKeys(user_type)
+
+def print_menu():
+    print()
+    print("Menu:\n")
     print("1: Add a new password")
     print("2: Retrieve a password")
-    print("3: Delete an existing password")
-    print("4: Generate a new password")
+    print("3: Delete an existing password (Code in progress)")
+    print("4: Generate a new password (Code in progress)")
     print("0: Exit")
-    choice = int(input("Enter a choice: "))
+    print()
+
+def get_user_choice():
+    while True:
+        try:
+            choice = int(input("Enter your choice: "))
+            if choice not in [0, 1, 2, 3, 4]:
+                raise ValueError("Invalid choice. Please enter a number from the menu.")
+            return choice
+        except ValueError as e:
+            pass
+
+def execute_choice(choice):
     match choice:
         case 1: 
-            addPassword(input("Enter a username/email/website: "), input("Enter a password to add: "))
+            addPassword()
         case 2: 
-            retrievePassword(input("Enter the username/email/website you need the password for: "))
+            retrievePassword()
         case 3:
-            print('Code in progress')
+            print("Deleting password... (Code in progress)")
         case 4:    
-            print('Code in progress')
+            print("Generating new password... (Code in progress)")
         case 0:
             sys.exit('Exiting...')
 
+###
 
-def addPassword(user, pwd):
+def addPassword():
+    user = input("Enter a username/email/website: ")
+    while True:    
+        try:
+            pwd = input("Enter a password to add: ")
+            if pwd != '':
+                break
+            else:
+                print("Password cannot be blank. Press Ctrl-d to go to main menu")
+        except EOFError:
+            main()    
     if validation(pwd):
+        encrypted_user = encrypt(user).decode('utf8')
+        encrypted_pwd = encrypt(pwd).decode('utf8')
         with open(pfile, 'a') as f:
-            f.write(f"user={encrypt(user).decode('utf8')}|,|pass={encrypt(pwd).decode('utf8')}\n")
+            f.write(f"{encrypted_user}|,|{encrypted_pwd}\n")
+        print("\nAdded!\n")
     main()
 
-def retrievePassword(userchoice):
+
+def retrievePassword():
     global masterPass
     lines = []
     with open(pfile, 'r') as f:
         for line in f:
             lines.append(line)
     if len(lines) > 0:
-        for current in lines:
-            user, pwd = current.replace('\n','').split('|,|')
-            user = user.replace('user=','')
-            pwd = pwd.replace('pass=','')
-            decUser= fernet.decrypt(user).decode('utf8')
-            if decUser == userchoice:
-                decPwd= fernet.decrypt(pwd).decode('utf8')
-                if input("Enter the master password: ") == masterPass:
-                    print(decUser)
-                    print(decPwd)
-            else:
-                print("Username not found")
+        userchoice = input("Enter the username/email/website you need the password for: ")
+        found = False
+        try:
+            for current in lines:
+                user, pwd = current.replace('\n','').split('|,|')
+                decUser= fernet.decrypt(user).decode('utf8')
+                if decUser == userchoice:
+                    decPwd= fernet.decrypt(pwd).decode('utf8')
+                    if input("Enter the master password: ") == masterPass:
+                        print()
+                        print(f"User: {decUser}")
+                        print(f"Password: {decPwd}")
+                        print()
+                        found = True
+                        break
+        except fernet.InvalidToken:
+            print("Fernet Token Error. Code needs debugging")
+
+
+        if not found: print("\nUsername not found\n")
+    else:
+        print("\nNo credentials currently stored.\n")
     main()
 
 
@@ -74,13 +123,6 @@ def decrypt(cipher):
     return decipher
     
 
-
-
-
-
-
-
-
 def authorize():            
     global masterFernet
     global masterPass
@@ -92,6 +134,7 @@ def authorize():
         print("Let's create your account")
         newUser = input("Enter a new username: ")
         newPass = input("Enter a new password: ")
+        masterPass = newPass
         encUser = masterFernet.encrypt(newUser.encode())
         encPass = masterFernet.encrypt(newPass.encode())
         with open(keydata, 'a') as f:
@@ -110,7 +153,7 @@ def authorize():
         masterPass = input("Enter a password: ")
         if user == decUser:
             if masterPass == decPwd:                  
-                print("Authorised\n")
+                print("\nAuthorised\n")
                 return 'existingUser'
             else:
                 sys.exit("\nWrong credentials, please try again\n")           #maybe give them another chance?
@@ -131,6 +174,7 @@ def initializeKeys(choice):
                 if 'key' in line:
                     key = line[5:]
                     fernet = Fernet(bytes(key[2:-1], encoding='utf8'))
+                    break
 
 def initMasterKey():
     global masterFernet
@@ -151,23 +195,25 @@ def initMasterKey():
 
 
 
-def validation(password):           ###
-    # lowercount = 0
-    # uppercount = 0
-    # numcount = 0
-    # for char in password:
-    #     if char >= 'a' and char <= 'z':
-    #         lowercount += 1
-    #     elif char >= 'A' and char <= 'Z':
-    #         uppercount += 1
-    #     elif char >= '0' and char <= '9':
-    #         numcount += 1
-            
-    # if len(password) < 8:
-    #     return False
-    # elif lowercount >= 1 and uppercount >= 1 and numcount >= 1:
-    #     return True
-    return True
+def validation(password):
+    if len(password) < 8:
+        print("\nWarning: Password must be of at least 8 characters\n")
+        return False
+
+    lowercount = sum(1 for char in password if char.islower())
+    uppercount = sum(1 for char in password if char.isupper())
+    numcount = sum(1 for char in password if char.isdigit())
+    if lowercount >= 1 and uppercount >= 1 and numcount >= 1:
+        print("\nSuccess: Password passes the security check\n")
+        return True
+    else:
+        while True:
+            inChoice = input("Warning: Password is not secure. Continue? (Y/N): ")
+            if inChoice == "Y":
+                return True
+            elif inChoice == "N":
+                break
+    # return True
 
 if __name__ == "__main__":
     main()
